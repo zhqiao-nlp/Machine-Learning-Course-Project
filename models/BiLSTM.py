e832e92e8f2e30c39a0e7c93b695b16f2127f97b
zhqiao-nlp <130669810+zhqiao-nlp@users.noreply.github.com>
2024-01-15 21:03:31 +0800

    Update BiLSTM.py

--- models/BiLSTM.py
+++ models/BiLSTM.py
@@ -12,17 +12,12 @@ class LSTMModel(nn.Module):
         super(LSTMModel, self).__init__()
         self.bidirectional_lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True,
                                           bidirectional=bidirectional)
-        # self.unidirectional_lstm = nn.LSTM(hidden_size * (2 if bidirectional else 1), hidden_size, num_layers,
-        #                                    batch_first=True, bidirectional=False)
-        self.bidirectional_lstm_2 = nn.LSTM(hidden_size * 2, hidden_size, num_layers, batch_first=True,
-                                          bidirectional=bidirectional)
+        # self.bidirectional_lstm_2 = nn.LSTM(hidden_size * 2, hidden_size, num_layers, batch_first=True,
+        #                                   bidirectional=bidirectional)
         self.fc = nn.Linear(hidden_size * 2, output_size)
 
     def forward(self, x):
         out_bidirectional, _ = self.bidirectional_lstm(x)
-        # out_unidirectional, _ = self.unidirectional_lstm(out_bidirectional)
-        # out_bidirectional_2, _ = self.bidirectional_lstm_2(out_bidirectional)
         out = F.tanh(out_bidirectional[:, -1, :])
-        # out = F.tanh(out_bidirectional_2[:, -1, :])
         out = self.fc(out)
         return out
