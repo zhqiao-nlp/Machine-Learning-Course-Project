abf420ac7089865f12fe092b7c2e8130ec6bc98b
zhqiao-nlp <130669810+zhqiao-nlp@users.noreply.github.com>
2024-01-15 20:59:47 +0800

    Add files via upload

--- utils/sliding_window.py
+++ utils/sliding_window.py
@@ -1,1 +1,9 @@
+import numpy as np
 
+
+def sliding_window(data, input_window, output_window, output_column):
+    train, label = [], []
+    for i in range(len(data) - input_window - output_window + 1):
+        train.append(data[i:i + input_window])
+        label.append(data[i + input_window:i + input_window + output_window, output_column])
+    return np.array(train), np.array(label)
